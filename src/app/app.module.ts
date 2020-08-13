69cfb1774f9735825292222c1539edf8ddad6c34
Darek <dardman88@gmail.com>
2020-08-13 15:38:00 +0200

    Create basic menus component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainBackgroundComponent } from './main-background/main-background.component';
+import { MenusComponent } from './menus/menus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MainBackgroundComponent
+    MainBackgroundComponent,
+    MenusComponent
   ],
   imports: [
     BrowserModule,
