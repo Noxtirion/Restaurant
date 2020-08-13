ed0a0bc40f620360f6ae397b5889a5e47489bbce
Darek <dardman88@gmail.com>
2020-08-13 17:36:11 +0200

    Add models folder with interface and data

---
+++ src/app/models/database.ts
@@ -0,0 +1,49 @@
+import { Product } from './product';
+
+export const BREAKFAST: Product[] = [
+  {
+    name: 'Scrambled eggs',
+    price: 10,
+    description: '3 eggs, salad, bread, butter, tomato'
+  },
+  {
+    name: 'Pancakes',
+    price: 8.5,
+    description: '2 pancakes with cheese or marmalade'
+  },
+  {
+    name: 'French Toasts',
+    price: 14,
+    description: '3 toasts with chocolate and fruits'
+  },
+  {
+    name: 'Banana Bread',
+    price: 8,
+    description: 'Sweet banana bread covered with chocolate'
+  },
+  {
+    name: 'English Breakfast',
+    price: 15.5,
+    description: '2 eggs, 3 sausages, tomato souce, 2 slices of dark bread'
+  },
+  {
+    name: 'Sunny Salad',
+    price: 12,
+    description: 'Salad with fresh fruits and vegetables'
+  },
+  {
+    name: 'Sandwiches',
+    price: 9,
+    description: '2 sandwitches with ham, cheese, tomato and cucumber'
+  },
+  {
+    name: 'Special Breakfast Meat',
+    price: 13.5,
+    description: 'Our special breakfast for meat lovers - ask waiter for details'
+  },
+  {
+    name: 'Special Breakfast Vege',
+    price: 12,
+    description: 'Our special breakfast for vegetable lovers - ask waiter for details'
+  }
+];
