eee03995ba504e75474977a958c3ace38e4d895b
Darek <dardman88@gmail.com>
2020-08-13 16:25:09 +0200

    Add router links and create basic about, contact and gallery components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { MenusComponent } from './menus/menus.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/menus', pathMatch: 'full' },
+  { path: 'menus', component: MenusComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact', component: ContactComponent }
+  //   { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainBackgroundComponent } from './main-background/main-background.component';
 import { MenusComponent } from './menus/menus.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     MainBackgroundComponent,
-    MenusComponent
+    MenusComponent,
+    AboutComponent,
+    GalleryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
