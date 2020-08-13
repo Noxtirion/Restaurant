7d68d481791b9e38eec83c05a3253f043d343cbe
Darek <dardman88@gmail.com>
2020-08-13 14:24:00 +0200

    Add background component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainBackgroundComponent } from './main-background/main-background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainBackgroundComponent
   ],
   imports: [
     BrowserModule,
