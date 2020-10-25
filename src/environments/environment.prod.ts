65381151c5babd4119f29a0fa30a3e2d4f70d98a
Darek <dardman88@gmail.com>
2020-10-25 13:27:21 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyC4AfCtZen0WqdOQPuXqI3kuDYvgOuEmU0',
+    authDomain: 'restaurant-angular-8c3c6.firebaseapp.com',
+    databaseURL: 'https://restaurant-angular-8c3c6.firebaseio.com',
+    projectId: 'restaurant-angular-8c3c6',
+    storageBucket: 'restaurant-angular-8c3c6.appspot.com',
+    messagingSenderId: '442650496220',
+    appId: '1:442650496220:web:846aec262c5a3db347b819',
   },
 };
