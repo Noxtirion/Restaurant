5dfff346cde0a2451ff62adf0bc658a6563b9afd
Darek <dardman88@gmail.com>
2020-10-25 14:03:08 +0100

    Change routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AuthGuardService } from './services/auth-guard-service/auth-guard.service';
 
 const routes: Routes = [
-  { path: '', component: MenusComponent },
+  { path: '', redirectTo: '/menus', pathMatch: 'full' },
   { path: 'menus', component: MenusComponent },
   { path: 'menu/:id', component: MenuDetailsComponent },
   { path: 'about', component: AboutComponent },
